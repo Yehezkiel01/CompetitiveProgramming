87ad5312db59386f2882f4284055423d0d87a6c9
Yehezkiel01 <39835686+Yehezkiel01@users.noreply.github.com>
2018-06-07 17:26:22 +0800

    Update Balkan-17-Cats.cpp

--- Balkan/Balkan-17-Cats.cpp
+++ Balkan/Balkan-17-Cats.cpp
@@ -1,3 +1,25 @@
+/*
+	Idea:
+	We need to reduce the problem into a simpler problem.
+	
+	First, assume there is no lion, and we group every consecutive cats and dogs. 
+	One of the observation is that if we move a cat or dog, we will also move all of their consecutive friends. 
+	
+	After that we only need to find a way to merge several group of cats and several group of dogs by moving them 
+	and also put some separator, the lion. Normally, putting seperator cost 1 movement, however, there are exception
+	for several consecutive cats or dogs who don't need additional cost to put the separator
+	because at first ,before any move was done, there is at least a lion besides the group.
+	
+	Do some precomputation to find the groups and also the special groups,groups that don't need additional cost 
+	to put separator before them. Then, we run dp with assumption that if we move a group X to join the group Y 
+	(a group that won't be moved), we may assume that we remove X from the array and it will be inserted somewhere save.
+	Be careful with the case that all of the cats or dogs are removed, it means that they couldn't join anyone, 
+	therefore there might be additional cost.
+	
+	My dp has 3 states, the position, the number of group that is not removed, and boolean that determine 
+	whether this is the first group or not (this is important because the transition will be affected).
+	In order to reduce the memory, we need to use flying table.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 #define pb push_back
